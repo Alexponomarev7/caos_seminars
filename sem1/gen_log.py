76721430e795e738b21b5925c7c880a6442d9feb
Alexponomarev7 <Alex.ponomarev7@ya.ru>
2020-09-01 19:00:49 +0300

    Update gen_log.py

--- sem1/gen_log.py
+++ sem1/gen_log.py
@@ -2,7 +2,7 @@ import time
 import random
 
 status = ["200 OK", "202 REDIRECT", "403 FORBIDDEN", "404 NOT FOUND", "500 INTERNAL ERROR"]
-user = ["root", "lexolordan", "tnya0", "ivan_arhipov", "guest", "vasya_morozov", "tema_lebedev", "edgar_shmavonyan"]
+user = ["root", "lexolordan", "ivan_arhipov", "guest", "vasya_morozov", "tema_lebedev", "edgar_shmavonyan"]
 for i in range(1000):
     status_i = random.randint(0, len(status) - 1)
     user_i = random.randint(0, len(user) - 1)
